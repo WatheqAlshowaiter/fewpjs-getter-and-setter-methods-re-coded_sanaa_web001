369dfb18fa1050e46b0228a0426f90fb838b7405
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-08-28 22:46:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your Circle class here
+
+class Circle {
+    constructor(radius) {
+    this.radius = radius;
+  }
+  
+  get diameter() {
+    return 2 * this.radius;
+  }
+  get circumference() {
+    return 2 * Math.PI * this.radius;
+  }
+  get area() {
+    return Math.PI * this.radius * this.radius;
+  }
+  
+//   set diameter(){
+
+//   }
+  set circumference(circumference){
+    this.circumference = circumference;
+  }
+//   set area(){
+
+//   }
+}
